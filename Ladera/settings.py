42e3f3c0012e409feeb24d0750235483ce45532e
LaderaJA <laderajohnalbert@gmail.com>
2024-10-10 15:46:52 +0800

    css changes

--- Ladera/settings.py
+++ Ladera/settings.py
@@ -106,8 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "ladera_app/static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
